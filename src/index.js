76d90dd73f49701c7fa29d354318c09d3f76b799
dax-tangco <daxaxis.tangco@volenday.com>
2023-02-15 00:02:44 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreatePost from './CreatePost';
+import Posts from './Posts';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/create/posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
